4e0e43994703ff08154f29a0e41576c1de673a92
BLevente103 <Bereg.Levente@C19-04>
2026-02-10 09:24:18 +0100

    - Added File Manager class

---
+++ ConsoleApp2/FileManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class FileManager
+    {
+        public static List<Pigs> ReadFile(string fileName)
+        {
+            List<Pigs> lines = new List<Pigs>();
+            try
+            {
+                foreach (string line in File.ReadAllLines(fileName, Encoding.UTF8).Skip(1))
+                {
+                    string[] temp = line.Split(';');
+                    if (temp.Length == 3)
+                    {
+                        lines.Add(new Pigs(temp[0], temp[1], double.Parse(temp[2]), int.Parse(temp[3])));
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return lines;
+        }
+    }
+}
